d365d00f0d0f6fbc4bead643c31e39a305356c8c
queenfonsa-arch <farhiyaomar@gmail.com>
2026-01-28 17:16:26 +0300

    done

---
+++ src/00-objectball.js
@@ -0,0 +1,64 @@
+// src/00-objectball.js
+
+// Objectball data structure
+function gameObject() {
+  return {
+    home: {
+      teamName: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        "Alan Anderson": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1,
+        },
+        "Reggie Evans": {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slamDunks: 7,
+        },
+        "Brook Lopez": {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 15,
+        },
+      },
+    },
+    away: {
+      teamName: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        "Jeff Adrien": {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 1,
+          slamDunks: 2,
+        },
+        "Bismak Biyombo": {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slamDunks: 10,
